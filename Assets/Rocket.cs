57d9d2b8d721b2eec20186e482a94bc472134d5e
Nufflee <zifcani@gmail.com>
2021-07-03 07:05:50 +0200

    Improve state management and make transition to Rigidbody state smooth.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
@@ -8,11 +9,18 @@ public enum CameraMode
   Follow
 }
 
+/*
+ * Rotations:
+ *   x -> -yaw
+ *   y -> -roll
+ *   z -> pitch
+ *   in that order, while Unity does z, x, y.
+ */
 public class Rocket : MonoBehaviour
 {
   private enum State
   {
-    //PadIdle,
+    PadIdle,
     PoweredFlight,
     Coasting,
     Freefall,
@@ -24,20 +32,24 @@ public class Rocket : MonoBehaviour
   public ParticleSystem FIRE;
   public ParticleSystem smokeParticles;
   public GameObject motor;
+  public float mass;
 
-  private Dictionary<string, List<string>> values = new Dictionary<string, List<string>>();
+  private readonly Dictionary<string, List<string>> values = new Dictionary<string, List<string>>();
   private int flightStartIndex;
+  private int flightEndIndex;
   private int flightCurrentIndex;
   private float flightStartTime;
   private new Camera camera;
   private new Rigidbody rigidbody;
-  private State state;
+  private State state = State.PadIdle;
 
   // Start is called before the first frame update
   private void Start()
   {
     camera = Camera.main;
-    rigidbody = GetComponentInChildren<Rigidbody>();
+    rigidbody = GetComponent<Rigidbody>();
+
+    rigidbody.mass = mass;
 
     Assert.IsNotNull(camera);
     Assert.IsNotNull(rigidbody);
@@ -61,19 +73,20 @@ public class Rocket : MonoBehaviour
       }
     }
 
-    Vector3 rocketPosition = transform.position;
-
-    if (cameraMode == CameraMode.Follow)
-    {
-      camera.transform.position = new Vector3(rocketPosition.x + 2f, rocketPosition.y + 1.5f, rocketPosition.z);
-    }
-    else if (cameraMode == CameraMode.Tracking)
-    {
-      camera.transform.position = new Vector3(10f, 1f, 0f);
-      camera.transform.LookAt(rocketPosition);
-    }
+    SetCameraPosition(transform.position);
 
     flightStartIndex = values["flightMicros"].FindIndex(value => int.Parse(value) > 0) - 1;
+
+    // Due to the data cutout, we have to end the file early to preserve the downward momentum
+    if (logFile.name == "flight3")
+    {
+      flightEndIndex = 850;
+    }
+    else
+    {
+      flightEndIndex = values["flightMicros"].Count - 1;
+    }
+
     flightCurrentIndex = flightStartIndex;
     flightStartTime = Time.time + 1;
   }
@@ -81,27 +94,15 @@ public class Rocket : MonoBehaviour
   // Update is called once per frame
   private void Update()
   {
+    if (state == State.Rigidbody)
+    {
+      SetCameraPosition(transform.position);
+
+      return;
+    }
+
     if (Time.time >= flightStartTime + float.Parse(values["flightMicros"][flightCurrentIndex]) / 1e6)
     {
-      Vector3 position;
-
-      if (state == State.Rigidbody)
-      {
-        position = transform.position;
-
-        if (cameraMode == CameraMode.Follow)
-        {
-          camera.transform.position = new Vector3(position.x + 2f, position.y + 1.5f, position.z);
-        }
-        else if (cameraMode == CameraMode.Tracking)
-        {
-          camera.transform.position = new Vector3(10f, 1f, 0f);
-          camera.transform.LookAt(transform.position);
-        }
-
-        return;
-      }
-
       float altitude = float.Parse(values["altitude"][flightCurrentIndex]);
       float yaw = float.Parse(values["yaw"][flightCurrentIndex]) * Mathf.Rad2Deg;
       float pitch = float.Parse(values["pitch"][flightCurrentIndex]) * Mathf.Rad2Deg;
@@ -111,36 +112,25 @@ public class Rocket : MonoBehaviour
       float accelerationX = float.Parse(values["aX"][flightCurrentIndex]);
       int internalState = int.Parse(values["state"][flightCurrentIndex]);
 
-      float thrust = accelerationX * 0.6f;
+      float thrust = accelerationX * mass;
 
       if (state == State.PoweredFlight)
       {
         ParticleSystem.MainModule mainModule = FIRE.main;
         mainModule.startSpeed = -thrust / 2;
       }
-      else
-      {
-        FIRE.Stop();
-        smokeParticles.Stop();
-      }
 
-      position = new Vector3(transform.position.x, altitude, transform.position.z);
+      Vector3 position = new Vector3(transform.position.x, altitude, transform.position.z);
 
-      transform.position = new Vector3(position.x, position.y + 0.5f, position.z);
+      transform.position = position;
 
-      if (cameraMode == CameraMode.Follow)
-      {
-        camera.transform.position = new Vector3(position.x + 2f, position.y + 1.5f, position.z);
-      }
-      else if (cameraMode == CameraMode.Tracking)
-      {
-        camera.transform.position = new Vector3(10f, 1f, 0f);
-        camera.transform.LookAt(transform.position);
-      }
+      SetCameraPosition(position);
 
-      transform.rotation = Quaternion.AngleAxis(-yaw, Vector3.right) *
-                           Quaternion.AngleAxis(-pitch, Vector3.back) *
-                           Quaternion.AngleAxis(-roll, Vector3.up);
+      Quaternion rotationAngleAxis = Quaternion.AngleAxis(-yaw, Vector3.right) *
+                                     Quaternion.AngleAxis(-pitch, Vector3.back) *
+                                     Quaternion.AngleAxis(-roll, Vector3.up);
+
+      transform.rotation = rotationAngleAxis;
 
       motor.transform.localRotation = Quaternion.Euler(-90, 0, 0) *
                                       Quaternion.AngleAxis(servoY, Vector3.back) *
@@ -148,36 +138,93 @@ public class Rocket : MonoBehaviour
 
       if (internalState <= 2)
       {
-        state = State.PoweredFlight;
+        SetState(State.PoweredFlight);
       }
       else if (internalState <= 3)
       {
-        state = State.Coasting;
+        SetState(State.Coasting);
       }
       else
       {
-        state = State.Freefall;
+        SetState(State.Freefall);
       }
 
       if (state == State.Freefall && altitude < 0.5f)
       {
-        state = State.Rigidbody;
-
-        rigidbody.isKinematic = false;
-        rigidbody.useGravity = true;
+        SetState(State.Rigidbody);
       }
 
-      if (flightCurrentIndex < values["altitude"].Count - 1)
+      if (flightCurrentIndex <= flightEndIndex)
       {
         flightCurrentIndex++;
       }
       else
       {
-        state = State.Rigidbody;
+        SetState(State.Rigidbody);
+      }
+    }
+  }
 
+  private void SetCameraPosition(Vector3 rocketPosition)
+  {
+    switch (cameraMode)
+    {
+      case CameraMode.Follow:
+        camera.transform.position = new Vector3(rocketPosition.x + 2f, rocketPosition.y + 1.5f, rocketPosition.z);
+        break;
+      case CameraMode.Tracking:
+        camera.transform.position = new Vector3(10f, 1f, 0f);
+        camera.transform.LookAt(rocketPosition);
+        break;
+      default:
+        throw new ArgumentOutOfRangeException(nameof(cameraMode), $"Unknown camera mode '{cameraMode}'");
+    }
+  }
+
+  private void SetState(State newState)
+  {
+    if (state == newState)
+    {
+      return;
+    }
+
+    Debug.Log($"Switching from state '{state}' to '{newState}'");
+
+    state = newState;
+
+    switch (newState)
+    {
+      case State.PoweredFlight:
+        FIRE.Play();
+        smokeParticles.Play();
+        break;
+      case State.Coasting:
+        FIRE.Stop();
+        smokeParticles.Stop();
+        break;
+      case State.Rigidbody:
         rigidbody.isKinematic = false;
         rigidbody.useGravity = true;
-      }
+
+        float lastAltitude = float.Parse(values["altitude"][flightCurrentIndex - 1]);
+        float currentAltitude = float.Parse(values["altitude"][flightCurrentIndex]);
+
+        float lastYaw = float.Parse(values["yaw"][flightCurrentIndex - 1]);
+        float currentYaw = float.Parse(values["yaw"][flightCurrentIndex]);
+        float lastPitch = float.Parse(values["pitch"][flightCurrentIndex - 1]);
+        float currentPitch = float.Parse(values["pitch"][flightCurrentIndex]);
+        float lastRoll = float.Parse(values["roll"][flightCurrentIndex - 1]);
+        float currentRoll = float.Parse(values["roll"][flightCurrentIndex]);
+
+        float dt = float.Parse(values["logDt"][flightCurrentIndex]) / 1e6f;
+
+        rigidbody.velocity = new Vector3(0, (currentAltitude - lastAltitude) / dt, 0);
+        rigidbody.angularVelocity = new Vector3(-(currentYaw - lastYaw) / dt, -(currentRoll - lastRoll) / dt, (currentPitch - lastPitch) / dt);
+        break;
+      case State.Freefall:
+        break;
+      default:
+        throw new ArgumentOutOfRangeException(nameof(newState), $"Unknown state '{newState}'");
     }
   }
 }
--- README.md
+++ README.md
@@ -5,18 +5,30 @@ Work in progress 3D model rocket visualizer program.
 ## TODO
 - [ ] Configuration UI
   - [ ] Model import
+    - [ ] .obj with texture/material data (.mtl)
+    - [ ] .fbx with bundled texture/material data
   - [ ] Data log file import
     - [ ] Standardized log file format
     - [ ] Ability to omit certain fields (like TVC data, orientation, inertial acceleration) and have some of them computed by the visualizer
     - [ ] Field remapper for non-standardized log files
   - [ ] Camera angle selection
   - [ ] Time scale control
-  - [ ] Timeline?
-    - [ ] Mark various events (burnout, apogee, time scale transitions) and state transitions (like for Rigidbody state)
 - [ ] Video file export
 - [ ] Better motor exhaust VFX
 - [ ] Better scenery
+  - Real life map data?
+  - Grass, clouds, other props etc.
+  - Launchpad
 - [ ] Parachutes
+- [ ] Clean up the Unity project
+
+### Stretch goals
+- [ ] Timeline?
+  - [ ] Mark various events (burnout, apogee, time scale transitions) and state transitions (like for Rigidbody state)
+- [ ] Multiple rockets
+- [ ] Scriptable rockets
+  - A standardized abstract/"frontend" API where CSV data would just be one of the "backends"
+  - Could even support scripting at runtime through C# CompilerServices or a different scripting language
 
 ## Credits
 - Rocket and Motor model by [ZegesMenden](https://github.com/zegesmenden)
